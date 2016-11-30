f4af366885a939704883fc6c467ca64fd041e11d
oscar <ocsuau@gmail.com>
2016-11-30 02:32:36 +0100

    Función magic extendida

--- Caesar.java
+++ Caesar.java
@@ -63,7 +63,9 @@ public class Caesar {
     }
 
     static String magic(String s) {
-        return null;
+        s = s.toUpperCase();
+        int num_provisional = encontrar_repetida(s);
+        return encontrar_mensaje(s, num_provisional);
     }
 
     //Función para recalcular delta.
@@ -73,5 +75,55 @@ public class Caesar {
             delta -= 26;
         }
         return delta;
+    }
+
+    static int encontrar_repetida(String s) {
+        int num_veces_mayor = 0;
+        int num_veces_actual = 0;
+        char caracter = '*';
+        for (int i = 0; i < s.length(); i++) {
+            if (caracter == s.charAt(i) || s.charAt(i) == ' ') {
+                continue;
+            }
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    num_veces_actual++;
+                }
+            }
+            if (num_veces_actual > num_veces_mayor) {
+                num_veces_mayor = num_veces_actual;
+                caracter = s.charAt(i);
+            }
+            num_veces_actual = 0;
+        }
+        return caracter;
+    }
+
+    static int calcular_delta_magic(int num_provisional, int letra_comun) {
+        if (num_provisional > letra_comun) {
+            return num_provisional - letra_comun;
+        }
+        return ('Z' - 64) - (letra_comun - num_provisional);
+    }
+
+    static String encontrar_mensaje(String s, int num_provisional) {
+        char[] letra_comun = {'E', 'I'};
+        StringBuilder retorno = new StringBuilder();
+        for (int i = 0; i < letra_comun.length; i++) {
+            retorno.append(Caesar.decypher(s, calcular_delta_magic(num_provisional, letra_comun[i])));
+            if (comprobar_coincidencias(retorno.toString())) {
+                return retorno.toString();
+            }
+            retorno.delete(0, retorno.length());
+        }
+        return null;
+    }
+
+    static boolean comprobar_coincidencias(String mensaje) {
+        if (mensaje.contains(" DE LA ") || mensaje.contains(" DE ") || mensaje.contains(" LA ") || mensaje.contains(" I ") || mensaje.contains(" ELS ")
+                || mensaje.contains(" LES ")) {
+            return true;
+        }
+        return false;
     }
 }
